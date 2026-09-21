7c3812878f4b1da452ba79db77580c0791e6d8d8
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 11:03:55 -0300

    chore(gen): converge projections after lane integration sweep

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -10,50 +10,20 @@ from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
     from flext_tests import c, d, e, h, m, p, r, s, t, td, tf, tk, tm, tv, u, x
-
-    from .test_smoke import TestsFlextLdapSmoke
 __all__: tuple[str, ...] = (
-    "TestsFlextLdapSmoke",
-    "c",
-    "d",
-    "e",
-    "h",
-    "m",
-    "p",
-    "r",
-    "s",
-    "t",
-    "td",
-    "tf",
-    "tk",
-    "tm",
-    "tv",
-    "u",
-    "x",
+    "c", "d", "e", "h",
+    "m", "p", "r", "s",
+    "t", "td", "tf", "tk",
+    "tm", "tv", "u", "x",
 )
 
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".test_smoke": ("TestsFlextLdapSmoke",),
             "flext_tests": (
-                "c",
-                "d",
-                "e",
-                "h",
-                "m",
-                "p",
-                "r",
-                "s",
-                "t",
-                "td",
-                "tf",
-                "tk",
-                "tm",
-                "tv",
-                "u",
-                "x",
-            ),
+                "c", "d", "e", "h", "m", "p", "r", "s", "t", "td", "tf", "tk", "tm",
+                "tv", "u", "x",
+            )
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
