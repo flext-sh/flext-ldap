3532ddf36a89a2186fa2a707ab50b4c5b78cfd5b
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 12:44:35 -0300

    fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,7 +13,6 @@ if TYPE_CHECKING:
 
     from . import integration, unit
     from .base import TestsFlextLdapServiceBase, TestsFlextLdapServiceBase as s
-    from .conftest import WorkerInputConfig
     from .constants import TestsFlextLdapConstants, TestsFlextLdapConstants as c
     from .models import TestsFlextLdapModels, TestsFlextLdapModels as m
     from .protocols import TestsFlextLdapProtocols, TestsFlextLdapProtocols as p
@@ -29,7 +28,6 @@ __all__: tuple[str, ...] = (
     "TestsFlextLdapSettings",
     "TestsFlextLdapTypes",
     "TestsFlextLdapUtilities",
-    "WorkerInputConfig",
     "c",
     "d",
     "e",
@@ -54,7 +52,6 @@ _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
             ".base": ("TestsFlextLdapServiceBase", "s"),
-            ".conftest": ("WorkerInputConfig",),
             ".constants": ("TestsFlextLdapConstants", "c"),
             ".integration": ("integration",),
             ".models": ("TestsFlextLdapModels", "m"),
