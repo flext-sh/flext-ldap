7babb837d6c4b8402e787e2498ba3a3aa61196ef
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 11:26:16 -0300

    chore(gen): converge projections with the published generator (d1c55b54f, one name per line)

--- src/flext_ldap/__init__.py
+++ src/flext_ldap/__init__.py
@@ -9,12 +9,18 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 from .__version__ import (
-    __author__ as __author__, __author_email__ as __author_email__, __description__ as __description__, __license__ as __license__,
-    __title__ as __title__, __url__ as __url__, __version__ as __version__, __version_info__ as __version_info__,
+    __author__ as __author__,
+    __author_email__ as __author_email__,
+    __description__ as __description__,
+    __license__ as __license__,
+    __title__ as __title__,
+    __url__ as __url__,
+    __version__ as __version__,
+    __version_info__ as __version_info__,
 )
 
 if TYPE_CHECKING:
-    from flext_ldif import d, e, h, r, x
+    from flext_core import d, e, h, r, x
 
     from . import adapters, services
     from .__version__ import FlextLdapVersion
@@ -30,15 +36,42 @@ if TYPE_CHECKING:
     from .typings import FlextLdapTypes, FlextLdapTypes as t
     from .utilities import FlextLdapUtilities, FlextLdapUtilities as u
 __all__: tuple[str, ...] = (
-    "FlextLdap", "FlextLdapApiRuntime", "FlextLdapConfig", "FlextLdapConstants",
-    "FlextLdapModels", "FlextLdapProtocols", "FlextLdapService", "FlextLdapSettings",
-    "FlextLdapSync", "FlextLdapTypes", "FlextLdapUtilities", "FlextLdapVersion",
-    "__author__", "__author_email__", "__description__", "__license__",
-    "__title__", "__url__", "__version__", "__version_info__",
-    "adapters", "c", "config", "d",
-    "e", "h", "ldap", "m",
-    "p", "r", "s", "services",
-    "settings", "t", "u", "x",
+    "FlextLdap",
+    "FlextLdapApiRuntime",
+    "FlextLdapConfig",
+    "FlextLdapConstants",
+    "FlextLdapModels",
+    "FlextLdapProtocols",
+    "FlextLdapService",
+    "FlextLdapSettings",
+    "FlextLdapSync",
+    "FlextLdapTypes",
+    "FlextLdapUtilities",
+    "FlextLdapVersion",
+    "__author__",
+    "__author_email__",
+    "__description__",
+    "__license__",
+    "__title__",
+    "__url__",
+    "__version__",
+    "__version_info__",
+    "adapters",
+    "c",
+    "config",
+    "d",
+    "e",
+    "h",
+    "ldap",
+    "m",
+    "p",
+    "r",
+    "s",
+    "services",
+    "settings",
+    "t",
+    "u",
+    "x",
 )
 
 _LAZY_IMPORTS = MappingProxyType(
@@ -58,7 +91,7 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services.sync": ("FlextLdapSync",),
             ".typings": ("FlextLdapTypes", "t"),
             ".utilities": ("FlextLdapUtilities", "u"),
-            "flext_ldif": ("d", "e", "h", "r", "x"),
+            "flext_core": ("d", "e", "h", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
